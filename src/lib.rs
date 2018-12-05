cf91e76483ac22ccf8b9d6c5f93097be59923397
Bownairo <eerokelly@gmail.com>
2018-12-05 11:06:21 -0500

    Update project code to use output class

--- src/lib.rs
+++ src/lib.rs
@@ -31,32 +31,46 @@ pub fn run(input: Vec<(i32, i32)>, output: &mut Output) -> Vec<(i32, i32)> {
 
     // Create a stack and push the first two points onto it
     let mut top_stack = Vec::new();
-    top_stack.push(iter.next().expect("points empty?"));
-    top_stack.push(iter.next().expect("points empty?"));
+    let a = iter.next().expect("points empty");
+    let b = iter.next().expect("points empty");
+    top_stack.push(a);
+    top_stack.push(b);
+    output.segs_add(a.x, a.y, b.x, b.y);
 
     // While left turn or only two points, pop stack
     for thing in iter {
-        while top_stack.len() >= 2 && Ordering::Greater != det(&thing, top_stack.last().expect("bad"), top_stack.get(top_stack.len() - 2).expect("bad")) {
+        while top_stack.len() >= 2 && Ordering::Greater != det(&thing, top_stack.last().expect("bad"), top_stack.get(top_stack.len() - 2).expect("bad"), output) {
             top_stack.pop();
+            output.segs_pop();
         }
         // Push if valid
+        let x = top_stack.last().unwrap().x;
+        let y = top_stack.last().unwrap().y;
         top_stack.push(thing);
+        output.segs_add(x, y, thing.x, thing.y);
     }
 
     // Reverse and repeat for the bottom hull
     let mut iter = points.iter().rev();
 
     let mut bottom_stack = Vec::new();
-    bottom_stack.push(iter.next().expect("points empty?"));
-    bottom_stack.push(iter.next().expect("points empty?"));
+    let a = iter.next().expect("points empty");
+    let b = iter.next().expect("points empty");
+    bottom_stack.push(a);
+    bottom_stack.push(b);
+    output.segs_add(a.x, a.y, b.x, b.y);
 
     // While left turn or only two points, pop stack
     for thing in iter {
-        while bottom_stack.len() >= 2 && Ordering::Greater != det(&thing, bottom_stack.last().expect("bad"), bottom_stack.get(bottom_stack.len() - 2).expect("bad")) {
+        while bottom_stack.len() >= 2 && Ordering::Greater != det(&thing, bottom_stack.last().expect("bad"), bottom_stack.get(bottom_stack.len() - 2).expect("bad"), output) {
             bottom_stack.pop();
+            output.segs_pop();
         }
         // Push if valid
+        let x = bottom_stack.last().unwrap().x;
+        let y = bottom_stack.last().unwrap().y;
         bottom_stack.push(thing);
+        output.segs_add(x, y, thing.x, thing.y);
     }
 
     // Pull off the first and last, because these are in both
@@ -80,7 +94,8 @@ pub fn run(input: Vec<(i32, i32)>, output: &mut Output) -> Vec<(i32, i32)> {
 }
 
 // Use det trick to find where a point lies compared to a line
-fn det(p: &Point, q: &Point, r: &Point) -> Ordering {
+fn det(p: &Point, q: &Point, r: &Point, output: &mut Output) -> Ordering {
+    output.rays_add(p.x, p.y, q.x, q.y);
     let val = (q.x * r.y - q.y * r.x) - p.x * (r.y - q.y) + p.y * (r.x - q.x);
     if val > 0 {
         Ordering::Greater
